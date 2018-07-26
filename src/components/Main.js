3e335c15bdf7e35bf0958cb585de5c60c1085194
jianjainlj <952139117@qq.com>
2018-07-26 11:09:33 +0800

    increase stage structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,27 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let imageDatas = require("../data/imageDatas.json");
+
+imageDatas = (function getImageURL(imageDatasArr) {
+  for(var i = 0,j = imageDatasArr.length;i<j;i++) {
+    var singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas)
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
