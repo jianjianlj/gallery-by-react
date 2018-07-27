9846826bac2ef4446ccabc0f1187e1d4ac60aaf9
jianjainlj <952139117@qq.com>
2018-07-27 14:08:54 +0800

    the image position

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,10 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+//获取图片相关的数据
 let imageDatas = require("../data/imageDatas.json");
-
+//利用自执行函数，将图片名信息转成图片URL路径信息
 imageDatas = (function getImageURL(imageDatasArr) {
   for(var i = 0,j = imageDatasArr.length;i<j;i++) {
     var singleImageData = imageDatasArr[i];
@@ -13,13 +14,207 @@ imageDatas = (function getImageURL(imageDatasArr) {
     imageDatasArr[i] = singleImageData;
   }
   return imageDatasArr;
-})(imageDatas)
+})(imageDatas);
+
+//获取区间内的一个随机值
+function getRangeRandom(low,high) {
+  return Math.ceil(Math.random() * (high - low) + low);
+}
+
+var ImgFigure = React.createClass({
+
+  render: function() {
+    let styleObj = {};
+
+    //如果props属性中制定了这张图的位置，则使用
+    if(this.props.arrange.pos) {
+      styleObj = this.props.arrange.pos;
+    }
+    return (
+      <figure className="img-figure" style={styleObj}>
+        <img src={this.props.data.imageURL}
+             alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2 className="img-title">
+            {this.props.data.title}
+          </h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
 
 class AppComponent extends React.Component {
+  constructor() {
+    super();
+    this.Constant = {
+      centerPos: {
+        left: 0,
+          right: 0
+      },
+      hPosRange: { //水平方向的取值范围
+        leftSecX: [0,0],
+        rightSecX: [0,0],
+        y: [0,0]
+      },
+      vPosRange: { //垂直方向的取值范围
+        x: [0,0],
+        topY: [0,0]
+      }
+    };
+    this.state = {
+      imgsArrangeArr: [
+        // {
+        //   pos: {
+        //     left: 0,
+        //     top: 0
+        //   },
+        //   rotate: 0, //旋转角度
+        //   isInverse: false, //正反面，false表示正面
+        //   isCenter: false //图片是否居中
+        // }
+      ]
+    }
+  };
+
+  //组件加载以后，为每张图片计算位置范围
+  componentDidMount () {
+
+    //首先拿到舞台的大小
+    var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+        stageW = stageDOM.scrollWidth,
+        stageH = stageDOM.scrollHeight,
+        halfStageW = Math.ceil(stageW / 2),
+        halfStageH = Math.ceil(stageH / 2);
+
+    //拿到一个imgFigure的大小
+    var imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+        imgW = imgFigureDOM.scrollWidth,
+        imgH = imgFigureDOM.scrollHeight,
+        halfImgW = imgW / 2,
+        halfImgH = imgH / 2;
+
+    //计算中心图片位置点
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      top: halfStageH - halfImgH
+    }
+
+    this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+    this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+    this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - halfImgH *3;
+    this.Constant.vPosRange.x[0] = halfStageW - halfImgW;
+    this.Constant.vPosRange.x[1] = halfStageW;
+
+    this.rearrange(0);
+
+  };
+
+  /*
+   * 重新布局所有图片
+   * @param centerIndex 指定居中排布那个图片
+   */
+  rearrange (centerIndex) {
+    let imgsArrangeArr = this.state.imgsArrangeArr,
+        Constant = this.Constant,
+        centerPos = Constant.centerPos,
+        hPosRange = Constant.hPosRange,
+        vPosRange = Constant.vPosRange,
+        hPosRangeLeftSecX = hPosRange.leftSecX,
+        hPosRangeRightSecX = hPosRange.rightSecX,
+        hPosRangeY = hPosRange.y,
+        vPosRangeTop = vPosRange.topY,
+        vPosRangeX = vPosRange.x,
+
+      imgsArrangeTopArr = [],
+      //上侧区域图片数量
+      topImgNum = Math.floor(Math.random() * 2),
+      topImgSpliceIndex = 0,
+      imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+
+    //首先居中centerIndex的图片，剧中图片给不需要旋转
+    imgsArrangeCenterArr[0] = {
+      pos: centerPos,
+      //旋转的标志位
+    }
+
+    //取出要布局上侧的图片的状态信息
+    topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+    imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum);
+
+    //布局位于上侧的图片
+    imgsArrangeTopArr.forEach(function(value,index){
+      imgsArrangeTopArr[index] = {
+        pos: {
+          top: getRangeRandom(vPosRangeTop[0],vPosRangeTop[1]),
+          left: getRangeRandom(vPosRangeX[0],vPosRangeX[1])
+        }
+      }
+    });
+
+    //布局两侧的照片
+    for(let i = 0,j = imgsArrangeArr.length,k = j / 2;i<j;i++) {
+      let hPosRangeLORX = null;
+
+      //前半部分布局左边，右半部分布局右边
+      if(i < k) {
+        hPosRangeLORX = hPosRangeLeftSecX;
+      }else {
+        hPosRangeLORX = hPosRangeRightSecX;
+      }
+
+      imgsArrangeArr[i] = {
+        pos: {
+          top: getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
+          left: getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
+        }
+      }
+    }
+
+    if(imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+      imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+    }
+    imgsArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+
+    this.setState({
+      imgsArrangeArr: imgsArrangeArr
+    })
+  };
+
   render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function(value,index) {
+
+      //初始化
+      if(!this.state.imgsArrangeArr[index]) {
+        this.state.imgsArrangeArr[index] = {
+          pos: {
+            left: 0,
+            top: 0
+          },
+          rotate: 0,
+          isInverse: false,
+          isCenter: false
+        }
+      }
+
+      imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]}/>);
+    }.bind(this));
+
     return (
-      <section className="stage">
-        <section className="img-sec"></section>
+      <section className="stage" ref="stage">
+        <section className="img-sec">
+          {imgFigures}
+        </section>
         <nav className="controller-nav"></nav>
       </section>
     );
